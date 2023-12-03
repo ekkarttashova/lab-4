2b17067346f0d45c6440a6bb47a116003f334414
Kate Kartashova <yekaterina_kartachova05@mail.ru>
2023-12-03 15:48:50 +0300

    Add files via upload

---
+++ lab 4.py
@@ -0,0 +1,63 @@
+backpack = []  #рюкзак
+total_weight = 0  #общий вес в рюкзаке
+initial_survival_points = 10 #изначальное количество очков
+max_backpack = 9 #максимальный размер рюкзака
+required_item = "i" #обязательный предмет
+total_survival_points = initial_survival_points  # Изначальное количество очков выживания
+inventory = [['empty' for _ in range(3)] for _ in range(3)]  # Двумерный "инвентарь"
+
+
+items = [
+    ('r', 3, 25),
+    ('p', 2, 15),
+    ('a', 2, 15),
+    ('m', 2, 20),
+    ('i', 1, 5),
+    ('k', 1, 15),
+    ('x', 3, 20),
+    ('t', 1, 25),
+    ('f', 1, 15),
+    ('d', 1, 10),
+    ('s', 2, 20),
+    ('c', 2, 20)
+]
+
+
+#сортировка предметов по их весу в возрастающем порядке и по их ценности в убывающем порядке
+sorted_items = sorted(items, key = lambda x: (x[1], -x[2]))
+
+
+#добавляем обязательный предмет
+for item in items:
+    if item[0] == required_item and total_weight + item[1] <= max_backpack:
+        backpack.append(item)
+        total_weight += item[1]
+        total_survival_points += item[2]
+        break
+
+#добавляем остальные предметы
+for item in sorted_items:
+    if item[0] != required_item and total_weight + item[1] <= max_backpack:
+        backpack.append(item)
+        total_weight += item[1]
+        total_survival_points += item[2]
+    else:
+        total_survival_points -= item[2]
+
+#заполняем "инвентарь"
+for i in range(len(backpack)):
+    x = i // 3
+    y = i % 3
+    inventory[x][y] = backpack[i][0]
+    inventory[2][0] = backpack[5][0]
+    inventory[2][1] = backpack[6][0]
+    inventory[2][2] = backpack[6][0]
+
+#проверка, что итоговое количество очков выживания положительно
+if total_survival_points > 0:
+    print("Итоговый инвентарь (3x3):")
+    for row in inventory:
+        print(row)
+    print(f"Итоговые очки выживания: {total_survival_points}")
+else:
+    print("Решение не найдено. Итоговое количество очков выживания неположительно.")
